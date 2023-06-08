2ab0e21d3418963ba19332affdc9713ba5be844f
tokai235 <t-horii@giftee.co>
2023-06-09 04:31:04 +0900

    スナップショットもリストアップ

--- list_snapshot.js
+++ list_snapshot.js
@@ -3,7 +3,7 @@ import {
 } from "@aws-sdk/client-ec2";
 import {
   ec2Client,
-  describeImagesRequestParams,
+  describeSnapshotsRequestParams,
   DELETE_SNAPSHOT_LIST_FILE,
   DELETE_TARGET_DATE,
 } from "./config.js";
@@ -15,18 +15,19 @@ let token = null;
 let deleteSnapshots = [];
 while (true) {
   const command = new DescribeSnapshotsCommand(
-    describeImagesRequestParams(token)
+    describeSnapshotsRequestParams(token)
   );
 
   const response = await ec2Client.send(command);
-  console.log(response.SnapShots[0].Name);
+  console.log(response.Snapshots[0].SnapshotId);
 
   // 削除対象のものを 名前と作成日 のみ抽出
-  const responseImages = response.SnapShots.filter(
-    (snapshot) => dayjs(snapshot.CreationDate) < DELETE_TARGET_DATE
+  const responseImages = response.Snapshots.filter(
+    (snapshot) => dayjs(snapshot.StartTime) < DELETE_TARGET_DATE
   ).map((snapshot) => ({
-    createdAt: snapshot.CreationDate,
-    snapshotName: snapshot.Name,
+    createdAt: snapshot.StartTime,
+    id: snapshot.SnapshotId,
+    description: snapshot.Description,
   }));
 
   deleteSnapshots.push(...responseImages);
