afafeb8b795e9a7148f0a89ec592fb179e6d3e37
tokai235 <t-horii@giftee.co>
2023-06-09 19:12:44 +0900

    待つ時間を短く

--- delete.js
+++ delete.js
@@ -4,6 +4,6 @@ import delete_snapshot from "./delete_snapshot.js";
 delete_ami()
 
 setTimeout(async () => {
-  // AMI が削除されないと EBS Snapshot は削除できないので、5秒後に実行する
+  // AMI が削除されないと EBS Snapshot は削除できないので、少し待って実行する
   await delete_snapshot()
-}, 5000)
+}, 1000)
