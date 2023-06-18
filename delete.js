37b02595f9a3d1f121dbbfe17748f51a1c62684c
tokai235 <t-horii@giftee.co>
2023-06-19 00:25:24 +0900

    非同期処理であることを明示

--- delete.js
+++ delete.js
@@ -1,7 +1,7 @@
 import delete_ami from "./delete_ami.js";
 import delete_snapshot from "./delete_snapshot.js";
 
-delete_ami()
+await delete_ami()
 
 setTimeout(async () => {
   // AMI が削除されないと EBS Snapshot は削除できないので、少し待って実行する
